d724b399b74f982608dc3ee092b7e1cd7213ad51
yulialener <lener.yu@yandex.ru>
2024-10-18 13:11:37 +0700

    Initial commit

---
+++ app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/IntentSettingsSecondWayTest.java
@@ -0,0 +1,47 @@
+package ru.kkuzmichev.simpleappforespresso;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasAction;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasData;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+import android.content.Intent;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.espresso.intent.rule.IntentsTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import io.qameta.allure.android.runners.AllureAndroidJUnit4;
+import ru.kkuzmichev.simpleappforespresso.MainActivity;
+import ru.kkuzmichev.simpleappforespresso.R;
+
+//@RunWith(AndroidJUnit4.class)
+@RunWith(AllureAndroidJUnit4.class)
+public class IntentSettingsSecondWayTest {
+
+    @Rule
+    public IntentsTestRule intentsTestRule =
+            new IntentsTestRule(MainActivity.class);
+
+    @Test
+    public void checkIntentFirstWay() {
+        ViewInteraction overflowMenuButton = onView(withContentDescription("More options"));
+        overflowMenuButton.check(matches(isDisplayed()));
+        overflowMenuButton.perform(click());
+        ViewInteraction settings = onView(allOf(withId(R.id.title), withText("Settings")));
+        settings.check(matches(isDisplayed()));
+        settings.perform(click());
+        intended(hasData("https://google.com"));
+        intended(hasAction(Intent.ACTION_VIEW));
+    }
+}
